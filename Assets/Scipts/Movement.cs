41cd43b66f07f6fbfbb70a4ef08e0970c1d8a21e
Silverfox22 <kai-gareis@outlook.de>
2022-11-05 20:38:20 +0100

    text

--- Assets/Scipts/Movement.cs
+++ Assets/Scipts/Movement.cs
@@ -1,14 +1,18 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Collider))]
 public class Movement : MonoBehaviour
 {
     private Camera mainCamera;
     private float CameraZDistance;
+    public int gridSize = 4;
     public float gridReselution = 1f;
     private bool collisionFront = true;
     private bool collisionBack = true;
+    //public GameObject YourCar;
+    //public Text myText;
 
     void Start()
     {
@@ -23,11 +27,13 @@ public class Movement : MonoBehaviour
             new Vector3(Input.mousePosition.x, Input.mousePosition.y, CameraZDistance); //z axis added to screen point 
         Vector3 NewWorldPosition =
             mainCamera.ScreenToWorldPoint(ScreenPosition); //Screen point converted to world point
-
         if (transform.localScale.y > transform.localScale.x)
         {
-                     
-            NewWorldPosition.y = gridPos(NewWorldPosition.y);
+
+            
+
+    
+            NewWorldPosition.y = gridPos(NewWorldPosition.y + gridReselution);
 
             if (collisionFront && NewWorldPosition.y > transform.position.y)
                 NewWorldPosition.y = transform.position.y;
@@ -35,11 +41,12 @@ public class Movement : MonoBehaviour
             if (collisionBack && NewWorldPosition.y < transform.position.y)
                 NewWorldPosition.y = transform.position.y;
 
+
             NewWorldPosition.x = transform.position.x;
         }
         else
         {
-            NewWorldPosition.x = gridPos(NewWorldPosition.x);
+            NewWorldPosition.x = gridPos(NewWorldPosition.x + gridReselution);
 
             if (collisionFront && NewWorldPosition.x < transform.position.x)
                 NewWorldPosition.x = transform.position.x;
@@ -49,14 +56,16 @@ public class Movement : MonoBehaviour
 
             NewWorldPosition.y = transform.position.y;
         }
-        transform.position = Vector3.MoveTowards(transform.position, NewWorldPosition, gridReselution/2);
-    }
-    float gridPos(float pos)
-    {   
-        if(pos < 0)
-            return 0f;
 
-        return Mathf.Round(pos / gridReselution) * gridReselution; ;
+        transform.position = NewWorldPosition;
+
+        //Win(transform.position.y); //but just for YourCar?
+    }
+
+    float gridPos(float pos)
+    {
+        float posistion = Mathf.Round(pos / gridReselution) * gridReselution;
+        return posistion;
     }
 
     public void CollisionDetectedFront()
@@ -70,4 +79,19 @@ public class Movement : MonoBehaviour
         collisionBack = !collisionBack;
         Debug.Log("collisionBack: " + collisionBack);
     }
+    //public void Win(float pos)
+    //{   
+        //if(pos <= 0)
+        //{
+            //myText.text = "You Won!";
+            //Debug.Log("You Won!");
+            //Console.WriteLine("You Won!");
+        //}
+        //GameObject i = Instantiate(YourCar) as GameObject;
+        //if(i.transform.position.y <= 0)
+        //{
+            //Console.WriteLine("You Won!");
+            //Debug.Log("You Won!");
+        //}
+    //}
 }
