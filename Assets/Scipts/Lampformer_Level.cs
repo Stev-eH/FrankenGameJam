010b0be07939661df44546a9bbe6c4317e708988
Kasamorra <81055522+Kasamorra@users.noreply.github.com>
2022-11-05 14:22:51 +0100

    Trying to add movement to the platforms now

---
+++ Assets/Scipts/Lampformer_Level.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Left-Right Movement of Platforms
+
+public class Lampformer_Level : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //NewWorldPosition.x = NewWorldPosition.x + 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
