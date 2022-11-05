c409d9e4368202394d6543b93d35ef2153dc3fa3
Kasamorra <81055522+Kasamorra@users.noreply.github.com>
2022-11-05 17:09:12 +0100

    Added movement for the platforms and the lamp. Still needs improvement

--- Assets/Scipts/MovementLampFormer.cs
+++ Assets/Scipts/MovementLampFormer.cs
@@ -1,7 +1,29 @@
+using System;
 using UnityEngine;
 
 public class MovementLampFormer : MonoBehaviour
 {
+    bool colliding = false; // variable to check whether object is colliding
+
+    //public void AddForce(Vector3 force, ForceMode mode = ForceMode.Force);
+
+    Rigidbody lamp_Rigidbody;
+    public float side_Thrust = 10f;
+    public float jump_Thrust = 50f;
+    public float max_velocity_side = 2f;
+    public float max_velocity_jump = 0.01f;
+
+    void OnCollisionEnter(Collision col)
+    {
+        colliding = true;
+        string collidingObject = col.body.gameObject.name; // reads what object lamp is colliding with
+    }
+
+    void OnCollisionExit ()
+    {
+        colliding = false;
+    }
+
     private float speed = 5.0f;
     public GameObject character;
 
@@ -9,22 +31,29 @@ public class MovementLampFormer : MonoBehaviour
     void Start()
     {
         speed = 5.0f;
+
+        //Fetch the Rigidbody from the GameObject with this script attached
+        lamp_Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && lamp_Rigidbody.velocity.x < max_velocity_side)
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            //transform.position += Vector3.right * speed * Time.deltaTime;
+            lamp_Rigidbody.AddForce(transform.right * side_Thrust);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && lamp_Rigidbody.velocity.x > -max_velocity_side)
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            //transform.position += Vector3.left * speed * Time.deltaTime;
+            lamp_Rigidbody.AddForce(-transform.right * side_Thrust);
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && lamp_Rigidbody.velocity.y < max_velocity_jump && lamp_Rigidbody.velocity.y > -max_velocity_jump)
         {
-            transform.position += Vector3.up * speed * Time.deltaTime;
+            //transform.position += Vector3.up * speed * Time.deltaTime *10;
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            lamp_Rigidbody.AddForce(transform.up * jump_Thrust);
         }
     }
 }
