5cd3b8cf88dcbd543e7da5d777b7d25657fc6454
Kasamorra <81055522+Kasamorra@users.noreply.github.com>
2022-11-05 21:00:58 +0100

    Collision Improved

--- Assets/Scipts/MoveLampFormer.cs
+++ Assets/Scipts/MoveLampFormer.cs
@@ -8,16 +8,27 @@ public class MoveLampFormer : MonoBehaviour
     //public void AddForce(Vector3 force, ForceMode mode = ForceMode.Force);
 
     Rigidbody lamp_Rigidbody;
+    public float speed = 0.1f;
+    public GameObject character;
     public float side_Thrust = 10f;
     public float jump_Thrust = 50f;
     public float max_velocity_side = 2f;
     public float max_velocity_jump = 0.01f;
     public string collidingObjectName = "none";
-    public bool collidingWithLeft = false;
-    public bool collidingWithRight = false;
-    public bool collidingWithPlatformsideleft = false;
-    public bool collidingWithPlatformsideright = false;
+    private bool collidingWithLeft = false;
+    private bool collidingWithRight = false;
+    private bool collidingWithPlatformsideleft = false;
+    private bool collidingWithPlatformsideright = false;
+    public float startingoffsetx = 0;
 
+    private void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.transform.parent.name == "Dynamic_Platform" && !collidingWithPlatformsideleft && !collidingWithPlatformsideright)
+        {
+            //Debug.Log("huhu");
+            transform.position += Vector3.right*((col.gameObject.transform.position.x - startingoffsetx) -transform.position.x);
+        }
+    }
     void OnCollisionEnter(Collision col)
     {
         colliding = true;
@@ -42,6 +53,12 @@ public class MoveLampFormer : MonoBehaviour
             }
             
         };
+        if (col.gameObject.transform.parent.name == "Dynamic_Platform" && !collidingWithPlatformsideleft && !collidingWithPlatformsideright)
+        {
+            //Collision with left-right moving platform, standing on it
+            startingoffsetx = col.gameObject.transform.position.x - transform.position.x;
+
+        }
     }
 
     void OnCollisionExit (Collision col)
@@ -59,14 +76,9 @@ public class MoveLampFormer : MonoBehaviour
 
     }
 
-    private float speed = 5.0f;
-    public GameObject character;
-
     // Start is called before the first frame update
     void Start()
     {
-        speed = 1.0f;
-
         //Fetch the Rigidbody from the GameObject with this script attached
         lamp_Rigidbody = GetComponent<Rigidbody>();
     }
